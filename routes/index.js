eb15f68eace95932ce07c0cfa84cd6489405cdf7
cogo79 <migobigo76@gmail.com>
2016-03-20 22:33:54 +0100

    Changed welcome page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '© Net Insight AB, load-balancer' });
 });
 
 module.exports = router;
